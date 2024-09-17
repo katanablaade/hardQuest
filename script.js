b0e392e7dc9d512b5c3cb96fc7333b8affdbe83c
Maksim Shubny <shubny.maksim@yandex.ru>
2024-09-17 20:28:46 +0500

    Внес правки: Добавил toLowerCase к возврату строки, поменял не строгое равенство на строгое, изменил массив на объект

--- script.js
+++ script.js
@@ -4,25 +4,25 @@
 // через if
 let lang = prompt("Выберите язык:", "ru/en");
 
-if (lang == "ru" || lang == "Ru" || lang == "RU") {
+if (lang.toLowerCase() === "ru") {
   console.log("пн, вт, ср, чт, пт, суб, вскр");
-} else if (lang == "en" || lang == "En" || lang == "EN") {
+} else if (lang.toLowerCase() === "en") {
   console.log("mn, ts, wd, th, fr, st, sn");
-} else if (lang == "ru/en") {
+} else if (lang === "ru/en") {
   console.log("Выберите один язык");
 } else {
   console.log("Упс, эти язык(-и) не поддерживается(-ются)");
 }
 
 // через switch-case
-switch (true) {
-  case lang == "ru" || lang == "Ru" || lang == "RU":
+switch (lang.toLowerCase()) {
+  case "ru":
     console.log("пн, вт, ср, чт, пт, суб, вскр");
     break;
-  case lang == "en" || lang == "En" || lang == "EN":
+  case "en":
     console.log("mn, ts, wd, th, fr, st, sn");
     break;
-  case lang == "ru/en":
+  case "ru/en":
     console.log("Выберите один язык");
     break;
   default:
@@ -31,20 +31,20 @@ switch (true) {
 }
 
 //через многомерный массив
-let array = [];
-
-array.ru = ["пн", "вт", "ср", "чт", "пт", "суб", "вскр"];
-array.en = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
-console.log(array[lang]);
+const array = {
+  ru: ["пн", "вт", "ср", "чт", "пт", "суб", "вскр"],
+  en: ["mn", "ts", "wd", "th", "fr", "st", "sn"],
+};
+console.log(array[lang.toLowerCase()]);
 
 //Задание 2
 
-let namePerson = prompt("Введите имя:", "Артем, Александр или др.");
+const namePerson = prompt("Введите имя:", "Артем, Александр или др.");
 
 console.log(
-  namePerson == "Артем"
+  namePerson === "Артем"
     ? "Директор"
-    : namePerson == "Александр"
+    : namePerson === "Александр"
     ? "Преподаватель"
     : "Студент"
 );
