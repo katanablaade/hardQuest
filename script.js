628704750d8012564ca222a01a8245513bf76aa6
Maksim Shubny <shubny.maksim@yandex.ru>
2024-09-19 04:48:52 +0500

    Complete quest05

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+"use strict";
+
 let screenPrice;
+let arr = ["251", "542", "367", "429", "891", "211", "777"];
+let numbers = 100;
 
 const isNumber = function (num) {
   return String(parseFloat(num)) === String(num) && isFinite(num);
@@ -8,4 +12,16 @@ do {
   screenPrice = prompt("Сколько будет стоить данная работа?");
 } while (!isNumber(screenPrice));
 
-console.log(screenPrice, typeof screenPrice);
+const results = arr.filter(function (digit) {
+  return digit.startsWith("2") || digit.startsWith("4");
+});
+
+primeNumbers: for (let i = 2; i < numbers; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue primeNumbers;
+  }
+  console.log(i + " Делители этого числа " + 1 + " и " + i);
+}
+
+console.log(screenPrice);
+console.log(results);
