28481c6d138d0b6b1a068606e3e52ec28e1713cc
Maksim Shubny <shubny.maksim@yandex.ru>
2024-09-20 22:01:40 +0500

    fix const names

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 
 let screenPrice;
-let arr = ["251", "542", "367", "429", "891", "211", "777"];
+let arr = ['251', '542', '367', '429', '891', '211', '777'];
 let numbers = 100;
 
 for (let i = 0; i < arr.length; i++) {
   const element = arr[i];
-  if (element[0] === "2" || element[0] === "4") {
+  if (element[0] === '2' || element[0] === '4') {
     console.log(element);
   }
 }
 
 for (let i = 0; i < numbers; i++) {
-  let kolichestvo = 0;
+  let counter = 0;
   for (let j = 0; j <= i; j++) {
     if (i % j === 0) {
-      kolichestvo++;
+      counter++;
     }
   }
-  if (kolichestvo === 2) {
-    console.log(i + " делители этого числа: " + 1 + " и " + i);
+  if (counter === 2) {
+    console.log(i + ' делители этого числа: ' + 1 + ' и ' + i);
   }
 }
